413b481fb6c8e5557018e671f10b225223c60e4a
Leon San José Larsson <leonlarsson8@gmail.com>
2023-04-11 12:17:59 +0200

    test

---
+++ app/image2/route.tsx
@@ -0,0 +1,62 @@
+import { ImageResponse } from "next/server";
+
+export default async () => {
+
+    try {
+        const res = await fetch("https://api.battlefieldstats.com");
+        const data = await res.json();
+
+        console.log(data);
+
+        return new ImageResponse(
+            (
+                <div
+                    style={{
+                        height: "100%",
+                        width: "100%",
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        backgroundColor: "#fff",
+                        fontSize: 32,
+                        fontWeight: 600
+                    }}
+                >
+                    <div>{data.totalGuilds} guilds</div>
+                    <div style={{ marginTop: 10 }}>{data.totalChannels} channels</div>
+                </div>
+
+            ),
+            {
+                width: 1200,
+                height: 600
+            }
+        );
+    } catch (error) {
+        return new ImageResponse(
+            (
+                <div
+                    style={{
+                        height: "100%",
+                        width: "100%",
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        backgroundColor: "#fff",
+                        fontSize: 32,
+                        fontWeight: 600
+                    }}
+                >
+                    <div style={{ color: "red" }}>Error fetching</div>
+                </div>
+
+            ),
+            {
+                width: 1200,
+                height: 600
+            }
+        );
+    }
+};
